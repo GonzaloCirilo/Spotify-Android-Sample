544ace510fa8ba9bdf39b845525d93a5e9f06f97
Gonzalo Cirilo <gonzalo_cirilo@hotmail.com>
2019-05-08 00:15:36 -0500

    Basic setup

--- README.md
+++ README.md
@@ -1,3 +1,16 @@
 # Spotify-Android-Sample
 Sample project integrating [Spotify API](https://developer.spotify.com/documentation/android/)
 
+## Steps
+* Download the Spotify Android SDK from the developers page
+* import the module into the project in Android Studio
+* Set up a new project in the [Spotify Developer Dashboard](https://developer.spotify.com/)
+* Once created Add the URI callback and Android Fingerprint to your project
+* Update the code in Main Activity to change the values for CLIENT_ID and REDIRECT_URI
+* Run the application
+
+NOTE: to see your SHA1 fingerprint of debug type `./gradlew` signingReport
+
+## Requirements
+* Android API 25
+* Spotify latest version installed
--- app/src/main/java/pe/com/redcups/spotify_android_sample/MainActivity.kt
+++ app/src/main/java/pe/com/redcups/spotify_android_sample/MainActivity.kt
@@ -2,11 +2,78 @@ package pe.com.redcups.spotify_android_sample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import com.spotify.android.appremote.api.ConnectionParams
+import com.spotify.android.appremote.api.Connector
+import com.spotify.android.appremote.api.SpotifyAppRemote
 
 class MainActivity : AppCompatActivity() {
+
+    val CLIENT_ID = "accf902e212f46dc9f8a157b18bef7ce"
+    val REDIRECT_URI = "testschema://callback"
+    private lateinit var mSpotifyAppRemote: SpotifyAppRemote
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onStart() {
+        super.onStart()
+
+        // Set up connection
+        val connectionParams = ConnectionParams.Builder(CLIENT_ID)
+            .setRedirectUri(REDIRECT_URI)
+            .showAuthView(true)
+            .build()
+
+        SpotifyAppRemote.connect(this, connectionParams,
+            object : Connector.ConnectionListener {
+
+                override fun onConnected(spotifyAppRemote: SpotifyAppRemote) {
+                    mSpotifyAppRemote = spotifyAppRemote
+                    Toast.makeText(baseContext,"Connected! Yay!",Toast.LENGTH_LONG).show()
+
+                    // Now you can start interacting with App Remote
+                    connected()
+
+                }
+
+                override fun onFailure(throwable: Throwable) {
+                    Toast.makeText(baseContext,"Error!",Toast.LENGTH_LONG).show()
+                    Log.e("MyActivity", throwable.message, throwable)
+                    // Something went wrong when attempting to connect! Handle errors here
+                }
+            })
+    }
+
+    fun connected() {
+        // We need the playlist ID
+        mSpotifyAppRemote.playerApi.play("spotify:playlist:2uFSXz1WeEUoZkP3jY5IHA")
+
+        // Subscribe to Observer to get information about current song playing track
+        // Can work well with Live Data or View Models
+        mSpotifyAppRemote.playerApi
+            .subscribeToPlayerState()
+            .setEventCallback { playerState ->  
+                val track = playerState.track
+                track?.let {
+                    Toast.makeText(
+                        baseContext,
+                        "${track.name} by ${track.artist.name}",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                    Log.d("MainActivity", "${track.name} by ${track.artist.name}")
+                }
+            }
+    }
+
+    override fun onStop() {
+        super.onStop()
+        // Disconnect when leaving the activity
+        if (::mSpotifyAppRemote.isInitialized){
+            SpotifyAppRemote.disconnect(mSpotifyAppRemote)
+        }
     }
 }
