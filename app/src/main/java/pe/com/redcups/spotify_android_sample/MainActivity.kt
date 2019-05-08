b2b46ae288ab2589655778d19c5b61430008088b
Gonzalo Cirilo <gonzalo_cirilo@hotmail.com>
2019-05-07 22:12:25 -0500

    initial commit

---
+++ app/src/main/java/pe/com/redcups/spotify_android_sample/MainActivity.kt
@@ -0,0 +1,12 @@
+package pe.com.redcups.spotify_android_sample
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
