066a6f91357bfe91c3761dcba26cf25995b025ba
Gonzalo Cirilo <gonzalo_cirilo@hotmail.com>
2019-05-10 09:38:32 -0500

    Added Track info and play button

--- app/src/main/java/pe/com/redcups/spotify_android_sample/MainActivity.kt
+++ app/src/main/java/pe/com/redcups/spotify_android_sample/MainActivity.kt
@@ -7,16 +7,32 @@ import android.widget.Toast
 import com.spotify.android.appremote.api.ConnectionParams
 import com.spotify.android.appremote.api.Connector
 import com.spotify.android.appremote.api.SpotifyAppRemote
+import com.spotify.protocol.types.ImageUri
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     val CLIENT_ID = "accf902e212f46dc9f8a157b18bef7ce"
     val REDIRECT_URI = "testschema://callback"
+    var isPlaying = false
     private lateinit var mSpotifyAppRemote: SpotifyAppRemote
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        play_button.setOnClickListener {
+            if (::mSpotifyAppRemote.isInitialized){
+                isPlaying = if(!isPlaying){
+                    mSpotifyAppRemote.playerApi.resume()
+                    !isPlaying
+                }else{
+                    mSpotifyAppRemote.playerApi.pause()
+                    !isPlaying
+                }
+            }else{
+                Toast.makeText(baseContext,"Not connected yet",Toast.LENGTH_LONG).show()
+            }
+        }
     }
 
     override fun onStart() {
@@ -50,7 +66,8 @@ class MainActivity : AppCompatActivity() {
 
     fun connected() {
         // We need the playlist ID
-        mSpotifyAppRemote.playerApi.play("spotify:playlist:2uFSXz1WeEUoZkP3jY5IHA")
+
+        //mSpotifyAppRemote.playerApi.play("spotify:playlist:2uFSXz1WeEUoZkP3jY5IHA")
 
         // Subscribe to Observer to get information about current song playing track
         // Can work well with Live Data or View Models
@@ -64,9 +81,21 @@ class MainActivity : AppCompatActivity() {
                         "${track.name} by ${track.artist.name}",
                         Toast.LENGTH_SHORT
                     ).show()
+                    song_text.text = "${track.name} by ${track.artist.name}"
+                    setArtwork(track.imageUri)
+
                     Log.d("MainActivity", "${track.name} by ${track.artist.name}")
                 }
+                isPlaying = !playerState.isPaused
             }
+
+
+    }
+
+    fun setArtwork(imageUri: ImageUri){
+        mSpotifyAppRemote.imagesApi.getImage(imageUri).setResultCallback {
+            track_art_work.setImageBitmap(it)
+        }
     }
 
     override fun onStop() {
